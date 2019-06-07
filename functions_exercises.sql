de0a407e4d02ec2468d9c79d26454aea75f4b7e1
John M De Leon <johnmdeleon894@gmail.com>
2019-06-07 12:25:17 -0500

    modified functions exercise.

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -15,7 +15,8 @@ select * from employees
 where first_name = 'irena'
    or first_name =  'Vidya'
    or first_name =  'Maya';
-# Add a condition to the previous query to find everybody with those names who is also male — 441 rows.
+ # Add a condition to the previous query to find everybody with those names who is also male —
+ # 441 rows.
 select * from employees
 where (first_name = 'irena'
     or first_name =  'Vidya'
@@ -25,7 +26,8 @@ where (first_name = 'irena'
 select * from employees
 where last_name like 'e%'
    or last_name like '%e' limit 0,10000000;
-# Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E' — 899 rows.
+ # Duplicate the previous query and update it to find all employees whose last name starts and
+ # ends with 'E' — 899 rows.
 select * from employees
 where last_name like 'e%'
   and last_name like '%e' limit 0,10000000;
@@ -37,13 +39,17 @@ where hire_date like '199%'
 select * from employees
 where last_name like '%q%'
   and last_name not like '%qu%';
-#Modify your first query to order by first name. The first result should be Irena Reutenauer and the last result should be Vidya Simmen.
+ #Modify your first query to order by first name. The first result should be Irena Reutenauer and
+ # the last result should be Vidya Simmen.
 select * from employees where first_name in ('irena', 'Vidya', 'Maya') order by first_name;
-#Update the query to order by first name and then last name. The first result should now be Irena Acton and the last should be Vidya Zweizig.
+# Update the query to order by first name and then last name. The first result should now be
+# Irena Acton and the last should be Vidya Zweizig.
 select * from employees where first_name in ('irena', 'Vidya', 'Maya') order by first_name, last_name;
-#Change the order by clause so that you order by last name before first name. Your first result should still be Irena Acton but now the last result should be Maya Zyda.
+#Change the order by clause so that you order by last name before first name. Your first result
+# should still be Irena Acton but now the last result should be Maya Zyda.
 select * from employees where first_name in ('irena', 'Vidya', 'Maya') order by last_name, first_name;
-#Update your queries for employees with 'E' in their last name to sort the results by their employee number. Your results should not change!
+ #Update your queries for employees with 'E' in their last name to sort the results by their
+ # employee number. Your results should not change!
 select * from employees
 where last_name like 'E%'
 order by emp_no
@@ -63,7 +69,8 @@ where last_name like 'e%'
    or last_name like '%e'
 order by emp_no desc
 limit 0,10000000;
-#Change the query for employees hired in the 90s and born on Christmas such that the first result is the oldest employee who was hired last. It should be Khun Bernini.
+# Change the query for employees hired in the 90s and born on Christmas such that the first
+# result is the oldest employee who was hired last. It should be Khun Bernini.
 select * from employees
 where hire_date like '199%'
   and birth_date like '%12-25'
